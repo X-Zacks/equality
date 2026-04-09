7a74450bd5b56434688eff6055f5c677cc5c807d
Equality Dev <equality-dev@lenovo.com>
2026-04-09 12:10:26 +0800

    修改启动命令

--- dev.cmd
+++ dev.cmd
@@ -15,7 +15,7 @@ set CUSTOM_MODEL=
 
 if exist "%~dp0.env.local" (
     for /f "usebackq tokens=1,* delims==" %%A in ("%~dp0.env.local") do (
-        if not "%%A"=="" if not "%%A:~0,1%"=="#" set "%%A=%%B"
+        set "%%A=%%B"
     )
 )
 
